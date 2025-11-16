cba3a6eccbd39f38dcc655745170826b1a44769e
Zulfadhli Zakari <zulfadhlizakari@gmail.com>
2025-11-17 01:51:17 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,6 @@
 export const CONFIG = {
 	delay: 10_000,
-	commandPrefix:
-		"cd /home/envs4/workspaces/tiktok-68/src && uv run main.py -no-update-check -mode automatic",
-	outputPath: "/mnt/c/users/envs4/downloads/Tiktok/",
+	commandPrefix: "cd ../src && uv run main.py -no-update-check -mode automatic",
+	outputPath: "/mnt/f/Tiktok/",
 	userListFile: "users.txt",
 } as const;
